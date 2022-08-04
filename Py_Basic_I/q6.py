4be1b6b8f0183780486bc85f7149c76e46335f55
Gabriel Vasconcelos <gabriels.v9@gmail.com>
2022-08-04 04:29:25 -0300

    new

---
+++ Py_Basic_I/q6.py
@@ -0,0 +1,4 @@
+l = input("Give me a list of number (EX: 4, 53, 9, 4): ")
+ttuple = l.rsplit(",")
+
+print(*ttuple)
---
+++ Py_Basic_I/q7.py
@@ -0,0 +1,4 @@
+s = "this is a new type of archive.java"
+
+x = s.split(".")
+print(f"This is a {x[-1]} file")
